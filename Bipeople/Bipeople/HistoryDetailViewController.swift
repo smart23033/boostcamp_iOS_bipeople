ef59db13d0ae73c9db236bf1bfbaeb5823efee99
김성준 <smart23033@gmail.com>
2017-08-17 17:44:17 +0900

    경로잇기 traces 가져와서 코드는 짰으나 실험을 못하는 상태.

--- Bipeople/Bipeople/HistoryDetailViewController.swift
+++ Bipeople/Bipeople/HistoryDetailViewController.swift
@@ -12,6 +12,8 @@ import GoogleMaps
 
 class HistoryDetailViewController: UIViewController {
     
+    //MARK: Outlets
+    
     @IBOutlet weak var titleLabel: UINavigationItem!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var ridingTimeLabel: UILabel!
@@ -20,12 +22,26 @@ class HistoryDetailViewController: UIViewController {
     @IBOutlet weak var maximumSpeedLabel: UILabel!
     @IBOutlet weak var caloriesLabel: UILabel!
     @IBOutlet weak var createdAt: UILabel!
+    @IBOutlet weak var mapView: GMSMapView!
     
-    var record : Record?
+    //MARK: Properties
+    
+    var record: Record?
+    var traces: [Trace]?
+    var navigationRoute: GMSPolyline?
+    
+    //MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        guard let recordID = record?._id  else {
+            return
+        }
+        
+        let predicate = NSPredicate(format: "recordID = %@", recordID)
+        traces = Array(RealmHelper.fetchFromType(of: Trace(), with: predicate))
+        
         titleLabel.title = "\(record?.departure ?? "unknown") - \(record?.arrival ?? "unknown")"
         distanceLabel.text = "\(record?.distance ?? 0) km"
         ridingTimeLabel.text = record?.ridingTime.stringTime
@@ -35,6 +51,29 @@ class HistoryDetailViewController: UIViewController {
         caloriesLabel.text = "\(record?.calories ?? 0) kcal"
         createdAt.text = record?.createdAt.toString()
         
+    }
+    
+    //MARK: Functions
+    
+    func drawRoute(from data: GeoJSON) {
+        let navigationPath = GMSMutablePath()
+        
+        traces?.forEach({ (trace) in
+            navigationPath.add(CLLocationCoordinate2D(latitude: trace.latitude, longitude: trace.longitude))
+        })
+        
+        navigationRoute = GMSPolyline(path: navigationPath)
+        
+        guard let route = navigationRoute else {
+            return
+        }
+        
+        route.strokeWidth = 5
+        route.strokeColor = UIColor.primary
+        
+        DispatchQueue.main.async {
+            route.map = self.mapView
+        }
     }
     
 }
