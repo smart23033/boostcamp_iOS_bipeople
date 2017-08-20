2f6427c4447562d78dfa89d4188645a754b7dce1
김성준 <smart23033@gmail.com>
2017-08-20 15:31:25 +0900

    Detail 에서 색상변경중

--- Bipeople/Bipeople/HistoryDetailViewController.swift
+++ Bipeople/Bipeople/HistoryDetailViewController.swift
@@ -13,6 +13,8 @@ import MarqueeLabel
 
 class HistoryDetailViewController: UIViewController {
     
+    static let MAX_COLOR_VALUE = 255.0
+    
     //MARK: Outlets
     
     @IBOutlet weak var titleLabel: UINavigationItem!
@@ -31,6 +33,13 @@ class HistoryDetailViewController: UIViewController {
     var traces: [Trace]?
     var navigationRoute: GMSPolyline?
     var marqueeTitle : MarqueeLabel?
+    
+    let navigationPath = GMSMutablePath()
+    var colorsAtCoordinate = [UIColor]()
+    
+    var redValue: CGFloat = 0.0
+    var greenValue: CGFloat = 0.0
+    var maxAltitude = 0.0
     
     //MARK: Life Cycle
     
@@ -51,7 +60,7 @@ class HistoryDetailViewController: UIViewController {
         titleLabel.titleView = marqueeTitle
         distanceLabel.text = "\(record?.distance.roundTo(places: 1) ?? 0) km"
         ridingTimeLabel.text = record?.ridingTime.stringTime
-        restTimeLabel.text = "\(record?.restTime ?? 0)"
+        restTimeLabel.text = record?.restTime.stringTime
         averageSpeedLabel.text = "\(record?.averageSpeed.roundTo(places: 1) ?? 0) m/s"
         maximumSpeedLabel.text = "\(record?.maximumSpeed.roundTo(places: 1) ?? 0) m/s"
         caloriesLabel.text = "\(record?.calories.roundTo(places: 1) ?? 0) kcal"
@@ -63,29 +72,55 @@ class HistoryDetailViewController: UIViewController {
     //MARK: Functions
     
     func drawRoute() {
-        let navigationPath = GMSMutablePath()
+        mapView.clear()
         
         traces?.forEach({ (trace) in
             navigationPath.add(CLLocationCoordinate2D(latitude: trace.latitude, longitude: trace.longitude))
+            
+            if trace.altitude > maxAltitude {
+                maxAltitude = trace.altitude
+            }
+            
+            redValue = CGFloat(trace.altitude / maxAltitude * HistoryDetailViewController.MAX_COLOR_VALUE)
+            greenValue = CGFloat(HistoryDetailViewController.MAX_COLOR_VALUE) - redValue
+            
+            colorsAtCoordinate.append(UIColor(red: redValue, green: greenValue, blue: 0, alpha: 1.0))
+            
         })
         
         navigationRoute = GMSPolyline(path: navigationPath)
         
-        guard let route = navigationRoute,
-            let firstTrace = traces?.first,
-            let traceCount = traces?.count,
-            let middleTrace = traces?[traceCount/2],
-            let lastTrace = traces?.last else {
-                return
+        var currentColor = UIColor()
+        
+        colorsAtCoordinate.forEach { (color) in
+            
+            guard color != colorsAtCoordinate.first else {
+                
+            }
+            
+            
+            currentColor = color
         }
         
         //경로 생성
-        route.map = nil
+        guard let route = navigationRoute else {
+            return
+        }
+        
         route.strokeWidth = 5
         route.strokeColor = UIColor.primary
         
         DispatchQueue.main.async {
             route.map = self.mapView
+        }
+        
+        //카메라 설정
+        guard let firstTrace = traces?.first,
+            let traceCount = traces?.count,
+            let middleTrace = traces?[traceCount/2],
+            let lastTrace = traces?.last else {
+                return
+                
         }
         
         mapView.camera = GMSCameraPosition.camera(
